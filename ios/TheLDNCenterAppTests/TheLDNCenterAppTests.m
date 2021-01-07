9501a7db1aee7466de40a55453a7f3cc3e2450b2
Joshua Becker <becker1@stolaf.edu>
2021-01-07 12:14:20 -0600

    First commit after rename

--- ios/TheLDNCenterAppTests/TheLDNCenterAppTests.m
+++ ios/TheLDNCenterAppTests/TheLDNCenterAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ChatAppTests : XCTestCase
+@interface TheLDNCenterAppTests : XCTestCase
 
 @end
 
-@implementation ChatAppTests
+@implementation TheLDNCenterAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
